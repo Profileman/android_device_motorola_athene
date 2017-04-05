eb0ee5998cb15bddcc5f07f4491b53ff3b46715b
Profileman <jmiller3342@gmail.com>
2017-04-05 10:56:50 -0400

    Update du.mk

--- du.mk
+++ du.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cosmic/config/common.mk)
+$(call inherit-product, vendor/du/config/common.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
