794b1b2df7fdf7497d82158ef65d37c04816bbcd
Profileman <jmiller3342@gmail.com>
2017-05-30 12:50:54 -0400

    Update and rename cos.mk to aosip.mk

--- aosip.mk
+++ aosip.mk
@@ -27,7 +27,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := cosmic_athene
+PRODUCT_NAME := aosip_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
